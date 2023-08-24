474c72dc8a81c748805501cc35a2bb1404e89cae
Aryan Kumar <57598770+Aryank728@users.noreply.github.com>
2023-08-24 16:54:13 +0530

    Update pdf to audiobook.py

--- pdf to audiobook.py
+++ pdf to audiobook.py
@@ -1,12 +1,30 @@
 import PyPDF2
 import pyttsx3
+import tkinter as tk
+from tkinter import filedialog
 
-path = open('C:\\Users\\Aryan\\Documents\\Aryan Resume.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(path)
+# Create a GUI window
+root = tk.Tk()
+root.withdraw()  # Hide the main window
 
-from_page = pdfReader.getPage(0)
-text = from_page.extractText()
+# Open a file picker dialog
+pdf_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
 
-speak = pyttsx3.init()
-speak.say(text)
-speak.runAndWait()
+try:
+    # Open the selected PDF file
+    path = open(pdf_path, 'rb')
+    pdfReader = PyPDF2.PdfReader(path)
+
+    from_page = pdfReader.pages[0]
+    text = from_page.extract_text()
+
+    speak = pyttsx3.init()
+    speak.say(text)
+    speak.runAndWait()
+
+except FileNotFoundError:
+    print("File not found.")
+except PyPDF2.utils.PdfReadError:
+    print("Error reading the PDF.")
+except Exception as e:
+    print("An error occurred:", e)
